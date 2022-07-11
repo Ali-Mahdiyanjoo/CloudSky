c34044d80c4bed5bd292fbb10edb309d0322dfd2
Professor <mahdiyanjooali@gmail.com>
2022-07-11 11:30:07 +0430

    The instaltion script has been added

--- Redis.sh
+++ Redis.sh
@@ -1,5 +1,6 @@
-CHOICE_SERVICE=$(whiptail --separate-output --checklist "Choose OS version" 20 120 5 \
-    "MongoDB" "MongoDB is a source-available cross-platform document-oriented database program." off \
-    "MySQL" "MySQL is an open-source relational database management system." off \
-    "PostgreSQL" "PostgreSQL, is a free and open-source relational database management system." off \ 
-    "Redis" "Redis is an open source nosql database that is in-memory data structure store." off \ 3>&1 1>&2 2>&3)
+#!/bin/bash
+
+sudo apt update
+sudo apt install redis-server
+sudo systemctl enable redis-server.service
+sydi systemctl start redis-server.service
